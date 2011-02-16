8966ca2e9bfc0c57b31aab3e66ff8219a40ad4e9
Vladymyr Kysenko <kysenko@viewdle.com>
2011-02-17 00:44:50 +0200

    ocl.go - added platform info method. sample.go - prints platform properties for all platforms

--- ocl.go
+++ ocl.go
@@ -5,7 +5,18 @@ package ocl
 
 */
 import "C"
+import "unsafe"
 
+//constants for clGetPlatformInfo function
+const (
+	CL_PLATFORM_PROFILE = C.CL_PLATFORM_PROFILE
+	CL_PLATFORM_VERSION = C.CL_PLATFORM_VERSION
+	CL_PLATFORM_NAME = C.CL_PLATFORM_NAME
+	CL_PLATFORM_VENDOR = C.CL_PLATFORM_VENDOR
+	CL_PLATFORM_EXTENSIONS = C.CL_PLATFORM_EXTENSIONS
+)
+
+type PlatformInfo C.cl_platform_info
 
 func PlatformsNumber() uint {
 	var numPlatforms C.cl_uint
@@ -14,19 +25,58 @@ func PlatformsNumber() uint {
 	return res
 }
 
-type Platform C.cl_platform_id
-type Device C.cl_device_id
-type Context C.cl_context
-type CommandQueue C.cl_command_queue
+type aPlatform C.cl_platform_id
+type Platform struct {
+	item aPlatform
+}
+
+type aDevice C.cl_device_id
+type Device struct {
+	item aDevice
+}
+
+type aContext C.cl_context
+type Context struct {
+	item aContext
+}
+
+type aCommandQueue C.cl_command_queue
+type CommandQueue struct {
+	item aCommandQueue
+}
+
 type Buffer C.cl_mem
 type Kernel C.cl_kernel
 type Event C.cl_event
 
+//type ContextProperty C.cl_context_property
+
 func Platforms(num uint) []Platform {
+	if num == 0 { num = PlatformsNumber() }
+	platforms := make([]aPlatform, num)
+	C.clGetPlatformIDs(C.cl_uint(num), (*C.cl_platform_id)(&platforms[0]), nil)
 	res := make([]Platform, num)
-	C.clGetPlatformIDs(C.cl_uint(num), (*C.cl_platform_id)(&res[0]), nil)
+	for i := 0; i < len(platforms); i++ {
+		res[i].item = platforms[i]
+	}
+
 	return res
 }
+
+
+func (pl *Platform) Info(pinfo PlatformInfo) string {
+	const bufSize = 4096
+	var bufReal C.size_t = 0
+
+	var cStr unsafe.Pointer = unsafe.Pointer(C.malloc(bufSize))
+	defer C.free(cStr)
+
+	C.clGetPlatformInfo(pl.item, C.cl_platform_info(pinfo), bufSize, cStr, &bufReal)
+	res := C.GoString((*C.char)(cStr))
+
+	return res
+}
+
 /*
 func CreateQueue(ctx *Context, dev *Device) (*CommandQueue) {
 	return nil
@@ -35,8 +85,11 @@ func CreateQueue(ctx *Context, dev *Device) (*CommandQueue) {
 func CreateContext() (*Context){
 	return nil
 }
-
-func (cq CommandQueue) Finish() {
-	C.clFinish(cq);
-}
 */
+
+
+func (cq *CommandQueue) Finish() {
+	C.clFinish(cq.item);
+}
+
+
